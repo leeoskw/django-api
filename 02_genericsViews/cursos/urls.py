febbe57095548ba4fdaa8b14983dcf33e62e093c
Leonardo <leeo_skw@hotmail.com>
2022-05-14 18:44:26 -0300

    Implementação do filtro por ID, DELETE e UPDATE através do RetrieveUpdateDestroy

--- 02_genericsViews/cursos/urls.py
+++ 02_genericsViews/cursos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('cursos/', CursoGenericView.as_view())
+    path('cursos/', CursoGenericView.as_view()),
+    path('cursos/<str:pk>/', Curso2GenericView.as_view())
 ]
--- 02_genericsViews/cursos/views.py
+++ 02_genericsViews/cursos/views.py
@@ -2,8 +2,9 @@ from rest_framework import generics
 from .models import *
 from .serializers import *
 
+# get (todos elementos) e POST em um novo elemento -> através do ListCreate
 
-class CursoGenericView(generics.ListCreateAPIView):
+class CursoGenericView(generics.ListCreateAPIView): 
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
@@ -11,3 +12,16 @@ class CursoGenericView(generics.ListCreateAPIView):
 class AvaliacaoGenericView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerialiazer
+
+
+
+# filtrando por um ID e DELETE -> através do RetrieveUpdateDestroy
+
+class Avaliacao2GenericView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerialiazer
+
+
+class Curso2GenericView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
